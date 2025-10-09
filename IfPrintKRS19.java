0685ad26945cf23377e31da09f0c3af18c1ceab9
Hisyam-Rana <rhn287@gmail.com>
2025-10-09 15:03:36 +0700

    Test Modification 1

--- IfPrintKRS19.java
+++ IfPrintKRS19.java
@@ -8,14 +8,19 @@ public class IfPrintKRS19 {
         System.out.print("Have the Tuition fees been paid in full ? (True/False) : ");
         boolean uktLunas = sc.nextBoolean();
 
-
+        // IF-ELSE Version
         if (uktLunas) {
             System.out.println("UKT Payment has been Verified");
             System.out.println("KRS now can be printed and you can ask the Academic Advisor to sign it.");   
         } else {
-            System.out.println("UKT Payment has not been Verified");
-            System.out.println("KRS can't be printed.");
+            System.out.println("Registration rejected. Please pay UKT first");
+            System.out.println("");
         }
 
+         // Ternary Operator Version
+        String message = uktLunas 
+                ? "UKT Payment has been verified\nKRS now can be printed and you can ask the academic advisor to sign it"
+                : "UKT Payment has not been verified";
+        System.out.println(message);
     }
 }
