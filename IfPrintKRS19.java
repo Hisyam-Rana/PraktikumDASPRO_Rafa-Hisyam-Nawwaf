539502caf764cb45ec68b9ed2f3a398c20a5144c
Hisyam-Rana <rhn287@gmail.com>
2025-10-09 12:45:38 +0700

    Task 1

--- IfPrintKRS19.java
+++ IfPrintKRS19.java
@@ -5,14 +5,16 @@ public class IfPrintKRS19 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("---Print KRS Siakad---");
-        System.out.println("Have the Tuition fees been paid in full ? (True/False) : ");
+        System.out.print("Have the Tuition fees been paid in full ? (True/False) : ");
         boolean uktLunas = sc.nextBoolean();
+
 
         if (uktLunas) {
             System.out.println("UKT Payment has been Verified");
-            System.out.println("KRS now can be printed and you can ask the Academic Advisor to sign it.");
-
-            
+            System.out.println("KRS now can be printed and you can ask the Academic Advisor to sign it.");   
+        } else {
+            System.out.println("UKT Payment has not been Verified");
+            System.out.println("KRS can't be printed.");
         }
 
     }
