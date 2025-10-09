00d26aee3e5eec909b82feec5c51dfaa390f418e
Hisyam-Rana <rhn287@gmail.com>
2025-10-09 11:22:55 +0700

    Attempt 1

---
+++ IfPrintKRS19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IfPrintKRS19 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Print KRS Siakad---");
+        System.out.println("Have the Tuition fees been paid in full ? (True/False) : ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("UKT Payment has been Verified");
+            System.out.println("KRS now can be printed and you can ask the Academic Advisor to sign it.");
+
+            
+        }
+
+    }
+}
