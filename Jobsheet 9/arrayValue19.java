e9431a89296dc1efe9baed41421f38ed4e2f4b99
Hisyam-Rana <rhn287@gmail.com>
2025-11-27 10:56:11 +0700

    Modified

--- Jobsheet 9/arrayValue19.java
+++ Jobsheet 9/arrayValue19.java
@@ -5,13 +5,14 @@ public class arrayValue19 {
         Scanner isc = new Scanner(System.in);
         int[] finalScore = new int[10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Enter The Final Score " + i + ": ");
+        for (int i = 0; i < finalScore.length; i++) {
+            System.out.print("Enter The Final Score " +i+ ": ");
             finalScore[i] = isc.nextInt();
-
-            for (i = 0; i < 10; i++) {
-                System.out.println("Final Score " + i + " is " + finalScore[i]);
-            }
         }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final Score " +i+ " is " + finalScore[i]);
+        }
+        
     }
 }
