3ceb6adbe3739af9cb58c79953ea660b39b0fa6e
Hisyam-Rana <rhn287@gmail.com>
2025-11-27 10:31:02 +0700

    ArrayNumbers19

--- Jobsheet 9/arrayNumbers19.java
+++ Jobsheet 9/arrayNumbers19.java
@@ -1,8 +1,7 @@
 public class arrayNumbers19 {
- 
+
     public static void main(String[] args) {
-        
-        int[] num = new int [4];
+        int[] num = new int[4];
 
         num[0] = 5;
         num[1] = 12;
@@ -13,8 +12,5 @@ public class arrayNumbers19 {
         System.out.println(num[1]);
         System.out.println(num[2]);
         System.out.println(num[3]);
-        
-
     }
 }
-
