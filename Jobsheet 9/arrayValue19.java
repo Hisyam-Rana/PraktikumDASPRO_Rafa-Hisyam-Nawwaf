d47a2075df35e39551b0df7a369d41eb82ec6518
Hisyam-Rana <rhn287@gmail.com>
2025-11-27 10:50:41 +0700

    ArrayValue19

--- Jobsheet 9/arrayValue19.java
+++ Jobsheet 9/arrayValue19.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 public class arrayValue19 {
-    public static void main(String[] args) {
-        
-        Scanner isc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
         int[] finalScore = new int[10];
-        for (int i = 0; i < finalScore.length; i++) {
-            System.out.print("Enter the final score " +i+ ": ");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter The Final Score " + i + ": ");
             finalScore[i] = isc.nextInt();
-        }
-        for (int i = 0; i < finalScore.length; i++) {
-            if (finalScore[i] > 70) {
-                System.out.println("Student " +i+ " Passed!");
+
+            for (i = 0; i < 10; i++) {
+                System.out.println("Final Score " + i + " is " + finalScore[i]);
             }
-            
         }
-    isc.close();
     }
 }
