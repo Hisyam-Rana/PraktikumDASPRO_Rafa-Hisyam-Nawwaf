d4ad5899e0422cf6581440fb08004ff4ab152cfb
Hisyam-Rana <rhn287@gmail.com>
2025-11-28 09:17:25 +0700

    Modified

--- Jobsheet 9/arrayAverageScore19.java
+++ Jobsheet 9/arrayAverageScore19.java
@@ -4,21 +4,44 @@ public class arrayAverageScore19 {
     public static void main(String[] args) {
         Scanner isc = new Scanner(System.in);
 
-        int[] score = new int[10];
-        double total = 0;
-        double avg;
+        int totalPassedScr = 0;
+        int totalFailedScr = 0;
+        int countPassed = 0;
+        int countFailed = 0;
+        double avgPassed = 0;
+        double avgFailed = 0;
+        
+        System.out.print("Enter the Number of Students : ");
+        int numStudents = isc.nextInt();
 
-        for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter Student Score " +( i + 1 )+ ": ");
-            score[i] = isc.nextInt();
+        int[] finalScores = new int[numStudents];
+
+        for (int i = 0; i < numStudents; i++) {
+            System.out.printf("Enter the Final Score %d : ", i);
+            finalScores[i] = isc.nextInt();
+        }
+
+        for (int score : finalScores) {
+            if (score >= 70) {
+                totalPassedScr += score;
+                countPassed++;
+            } else {
+                totalFailedScr += score;
+                countFailed++;
+            }
         }
         
-        for (int i = 0; i < score.length; i++) {
-            total += score[i];
+        if (countPassed > 0) {
+            avgPassed = (double) totalPassedScr / countPassed;
         }
 
-        avg = total / score.length;
-        System.out.println("The Class average Score is " + avg);
+        if (countFailed > 0) {
+            avgFailed = (double) totalFailedScr / countFailed;
+        }
 
+        System.out.println("The average score of students who passed is : " + avgPassed);
+        System.out.println("The average score of students who failed is : " + avgFailed);
+    
+        isc.close();
     }
 }
