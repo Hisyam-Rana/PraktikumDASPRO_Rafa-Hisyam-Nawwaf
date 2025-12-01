311e628a7ddf6b20a1d16ea4ef2b53dd9b3de111
Hisyam-Rana <rhn287@gmail.com>
2025-12-01 07:34:31 +0700

    Modified 2

--- Jobsheet 10/cinema19.java
+++ Jobsheet 10/cinema19.java
@@ -11,16 +11,39 @@ public class cinema19 {
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
 
-        System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
-        System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
-        System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
-        System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
-
         System.out.println(audience.length);
-        System.out.println(audience[0].length);
-        System.out.println(audience[1].length);
-        System.out.println(audience[2].length);
-        System.out.println(audience[3].length);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Length of row " + (i+1) + ": " + audience[i].length);
+        }
+
+        System.out.println("Audience in the row 3 : ");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.println(audience[2][i]);
+        }
+
+        System.out.println("Audience in the row 3 : ");
+        for (String i : audience[2]) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row : " + (i+1) + ". " + String.join(", ", audience[i]));
+        }
+
+        //for (String[] rowAudience : audience) {
+        //    System.out.println("Length of row : " + rowAudience.length);
+        //}
+
+        //System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
+        //System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
+        //System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
+        //System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
+
+        //System.out.println(audience.length);
+        //System.out.println(audience[0].length);
+        //System.out.println(audience[1].length);
+        //System.out.println(audience[2].length);
+        //System.out.println(audience[3].length);
         
     }
 }
