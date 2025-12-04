b673bce463353f198b8a2c8c2b2b79388f3fdb1e
Hisyam-Rana <rhn287@gmail.com>
2025-12-04 16:01:33 +0700

    Experiment 1

---
+++ Jobsheet 11/gratitude_19.java
@@ -0,0 +1,14 @@
+public class gratitude_19 {
+    
+    public static void sayThankYou() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+            "You inspired in me a love for learning and made me feel like i could ask you anything");
+    }
+
+    public static void main(String[] args) {
+        sayThankYou();
+    }
+    
+    
+}
+
