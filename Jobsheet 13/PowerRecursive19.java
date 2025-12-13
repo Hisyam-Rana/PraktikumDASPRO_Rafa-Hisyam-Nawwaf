397431753db8e6cfcbe13e46fcd208f1e7705e54
Hisyam-Rana <rhn287@gmail.com>
2025-12-13 22:44:08 +0700

    Modified

--- Jobsheet 13/PowerRecursive19.java
+++ Jobsheet 13/PowerRecursive19.java
@@ -11,19 +11,24 @@ public class PowerRecursive19 {
         System.out.print("Input Power Number : ");
         int power = isc.nextInt();
 
-        System.out.println("Result of " +base+ " power " +power+ " = " +
+        System.out.println("\nResult of " +base+ " power " +power+ " = " +
                                             calculatePower(base, power));
-
+        for (int i = 1; i <= power; i++) {
+                System.out.print(base + "x" );
+            }
+        System.out.print("1");
+        System.out.print(" = " + calculatePower(base, power));
         isc.close();
     }
 
     static int calculatePower (int base, int pow) {
+        int total;
+        
         if (pow == 1) {
-            return 1;
-        } else {
-            return base * calculatePower(base, pow-1);
+            return base;
         }
+
+        total = base * calculatePower(base, pow-1);
+        return total;
     }
-
-
 }
